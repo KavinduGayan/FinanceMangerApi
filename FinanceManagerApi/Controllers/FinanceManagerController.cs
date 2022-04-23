27b244f7e973664890050d08732d3c2e82cf7d5e
Kavindu Gayan <kavrup93@gmail.com>
2022-04-23 13:31:16 +0530

    first commit

---
+++ FinanceManagerApi/Controllers/FinanceManagerController.cs
@@ -0,0 +1,27 @@
+﻿using FinanceManagerApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FinanceManagerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult getAllTransactions()
+        {
+            List<Transaction> transactions = new List<Transaction>();
+            Transaction trans = new Transaction();
+            trans.amount = 10;
+            trans.description = "test";
+            transactions.Add(trans);
+            return Ok(transactions);
+        }
+        /*[HttpPost]
+        public IActionResult saveTransaction()
+        {
+            return Ok(trans);
+        }*/
+    }
+}
---
+++ FinanceManagerApi/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿namespace FinanceManagerApi.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public Double amount { get; set; }
+        public String description { get; set; }
+        public String transType { get; set; }
+        public Boolean recurring { get; set; }
+
+        public DateTime insertedDate { get; set; }
+
+        public DateTime? transactionDate { get; set; }
+    }
+}
